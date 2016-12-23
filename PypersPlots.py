Layout fixed for interactive mode. Gradient example added.

PypersPlots.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python -tt
 
-def figsize(scale,landscape=True):
+def figsize(scale,landscape=True,ratio=None):
     import numpy as np
     fig_width_pt = 469.755                          # Get this from LaTeX using \the\textwidth
     inches_per_pt = 1.0/72.27                       # Convert pt to inch
-    golden_mean = (np.sqrt(5.0)-1.0)/2.0            # Aesthetic ratio (you could change this)
+    if ratio is None:
+        golden_mean = 2.0/(np.sqrt(5.0)-1.0)        # Aesthetic ratio (you could change this)
+        ratio = golden_mean
     if landscape:
         fig_width = fig_width_pt*inches_per_pt*scale    # width in inches
-        fig_height = fig_width*golden_mean              # height in inches
+        fig_height = fig_width/ratio              # height in inches
     else:
         fig_width = fig_width_pt*inches_per_pt*scale    # width in inches
-        fig_height = fig_width/golden_mean              # height in inches
+        fig_height = fig_width*ratio              # height in inches
     fig_size = [fig_width,fig_height]
     return fig_size
 
-def latexify(fscale=1.0):
+def latexify(fscale=1.0,ratio=None):
     """Define the matplotlib preamble.
 
     The PGF preamble corresponds to the latex preamble in MNRAS class. It
     can be modified to whatever LaTeX preable needed.
     """
     import matplotlib as mpl
-    mpl.use('pgf')
     from matplotlib.backends.backend_pgf import FigureCanvasPgf
     mpl.backend_bases.register_backend('pdf', FigureCanvasPgf)
     mpl.backend_bases.register_backend('png', FigureCanvasPgf)
+    mpl.backend_bases.register_backend('pgf', FigureCanvasPgf)
     rc_mnras_preamble = {
-        #"backend": "pgf"
         "text.usetex": False,        # use LaTeX to write all text
         "axes.labelsize": 16,        # fontsize for x and y labels (was 10)
         "legend.fontsize": 10,       # was 10
-        "xtick.labelsize": 10,
-        "ytick.labelsize": 10,
-        "figure.figsize": figsize(fscale),     # default fig size of 0.9 textwidth
+        "figure.figsize": figsize(fscale,ratio=ratio),  # default fig size of 0.9 textwidth
         "figure.autolayout": True,
         "savefig.transparent": True,
         "savefig.dpi": 300,
@@ -51,7 +50,7 @@ def latexify(fscale=1.0):
     }
     mpl.rcParams.update(rc_mnras_preamble)
 
-def initPlot(nrows=1,ncols=1,landscape=True):
+def initPlot(nrows=1,ncols=1,landscape=True,fscale=1.0,ratio=None):
     """Initializing plot.
 
     Return:
@@ -59,38 +58,54 @@ def initPlot(nrows=1,ncols=1,landscape=True):
     * fig: Figure class.
 
     """
-    import matplotlib.pyplot as plt
+    latexify(fscale=fscale,ratio=ratio)
     from matplotlib import figure
+    import matplotlib.pyplot as plt
 
-    if landscape:
-        w,h = figure.figaspect(2.0/3.0)
-    else:
-        w,h = figure.figaspect(1.5)
-    plt.clf()
+    wh = plt.rcParams['figure.figsize']
+    plt.close('all')
     fig, ax = plt.subplots(nrows=nrows, ncols=ncols)
-    fig.set_size_inches(w, h)
+    fig.set_size_inches(wh[0],wh[1])
 
     return fig,ax
 
-def initGrid(nrows=1,ncols=1,cbloc="right",cbmode="each",landscape=True):
+def initGrid(nrows=1,ncols=1,cbloc="right",cbmode="each",landscape=True,fscale=1.0,ratio=None):
+    """
+    Function that generates the grid of plots.
+
+    Parameters
+    ----------
+    nrows : int
+    ncols : int
+    cbloc : str
+            right, top
+    cbmode : str
+    landscape : bool
+
+    Returns
+    -------
+    fig: Figure
+    ax : array_like
+    """
+    latexify(fscale=fscale,ratio=ratio)
     import matplotlib.pyplot as plt
+    from matplotlib import figure
     from mpl_toolkits.axes_grid1 import AxesGrid
 
-    if landscape:
-        w,h = figure.figaspect(2.0/3.0)
-    else:
-        w,h = figure.figaspect(1.5)
-    fig = plt.figure(figsize=(w, h))
+    wh = plt.rcParams['figure.figsize']
+    plt.close('all')
 
+    fig = plt.figure(figsize=(wh[0],wh[1]))
     grid = AxesGrid(fig, 111,
                     nrows_ncols=(nrows, ncols),
-                    axes_pad=0.0,
+                    add_all=True,
+                    axes_pad=0.,
                     share_all=True,
                     label_mode="L",
                     cbar_location=cbloc,
-                    cbar_mode=cbmode
+                    cbar_mode=cbmode,
     )
-    return grid
+    return fig, grid
 
 def decor(ax,xlim=None,ylim=None,xlabel=r"$x$",ylabel=r"$y$",lw=1.0,tlabelsize=10,minticks_on=True,gridon=False):
     if minticks_on:
@@ -100,9 +115,9 @@ def decor(ax,xlim=None,ylim=None,xlabel=r"$x$",ylabel=r"$y$",lw=1.0,tlabelsize=1
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
 
-    if xlim == None:
+    if xlim is None:
         xlim = ax.get_xlim()
-    if ylim == None:
+    if ylim is None:
         ylim = ax.get_ylim()
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
@@ -115,10 +130,11 @@ def decor(ax,xlim=None,ylim=None,xlabel=r"$x$",ylabel=r"$y$",lw=1.0,tlabelsize=1
         ax.grid()
 
 def printer(fig,fname, savedir="./", onscreen=False, rasterd=True, printPNG=False, printPDF=True, delPNG=True):
-    import matplotlib.pyplot as plt
     if onscreen:
+        #import matplotlib.pyplot as plt
         fig.suptitle(fname)
-        plt.show()
+        fig.set_tight_layout({'pad':0.1})
+        fig.show()
     else:
         import subprocess as sp
         import os
@@ -156,11 +172,12 @@ def printer(fig,fname, savedir="./", onscreen=False, rasterd=True, printPNG=Fals
 
     print('  Printing: done')
 
+## The contour plots
 def theContours(ax, v1, v2, t, numl=None, clabels=False, logsep=False, fmt='%1.0f', levs=[1], labelpos=[0], rasterd=True, colors='k'):
     """ Setting the contour lines.
     """
     import numpy as np
-    if numl==None:
+    if numl is None:
         if len(levs) == 1 and levs[0] == 1:
             CS = ax.contour(v1, v2, t, colors=colors, lw=1.0, rasterized=rasterd)
         else:
@@ -179,49 +196,64 @@ def theContours(ax, v1, v2, t, numl=None, clabels=False, logsep=False, fmt='%1.0
             ax.clabel(CS, fontsize=10, inline=True, fmt=fmt, manual=labelpos)
     return CS
 
-def theGradient(fig, ax, v1, v2, t, cbmin, cbmax, v1label=r"$x$", v2label="$y$", tlabel="$z$", LNorm=True, cmap=None, xlim=(0.0,3.0), ylim=(0.0,3.0), rasterd=True):
+def theGradient(ax, v1, v2, t, cmlim, v1label=r"$x$", v2label="$y$", tlabel="$z$", LNorm=True, cmap=None, xlim=(0.0,3.0), ylim=(0.0,3.0), rasterd=True):
     """Setting the contour gradient plot.
     """
-    if cmap == None:
+    import matplotlib.colors as col
+
+    if cmap is None:
         import colorcet as cc
         cmap = cc.cm['bgy']
+
+    cm_min = cmlim[0]
+    cm_max = cmlim[1]
+
     if LNorm:
         CM = ax.pcolormesh(v1, v2, t,
                            cmap=cmap,
-                           norm=col.LogNorm(vmin=cbmin, vmax=cbmax),
+                           norm=col.LogNorm(vmin=cm_min, vmax=cm_max),
                            rasterized=rasterd
         )
     else:
         CM = ax.pcolormesh(v1, v2, t,
                            cmap=cmap,
-                           norm=col.Normalize(vmin=cbmin, vmax=cbmax),
+                           norm=col.Normalize(vmin=cm_min, vmax=cm_max),
                            rasterized=rasterd
         )
     return CM
 
-def setColorBar(TT,fig,ax,log=False,blw=1.0,cblabel=r"$z$",subs=[1.0]):
+def setColorBar(TT,fig,cbax,log=False,blw=1.0,cblabel=r"$z$",subs=[1.0],**kw):
     import matplotlib.colors as col
     import matplotlib.ticker as ticker
+    from mpl_toolkits.axes_grid1.axes_grid import CbarAxes
+
+    if type(cbax) == CbarAxes:
+        ax = None
+        cax = cbax
+    else:
+        ax = cbax
+        cax = None
+        kw = {'pad': 0.0}
 
     if log:
         cbticks = ticker.LogLocator(base=10.0, subs=subs)
-        CB = fig.colorbar(TT, ax=ax,
-                          pad=0.0,
+        CB = fig.colorbar(TT, cax=cax, ax=ax,
+                          extendrect = True,
+                          extend = 'both',
+                          extendfrac = 0.01,
+                          ticks = cbticks,
+                          format = ticker.LogFormatter(base=10.0, labelOnlyBase=False),
+                          **kw
+        )
+    else:
+        CB = fig.colorbar(TT, cax=cax, ax=ax,
                           extendrect=True,
                           extend='both',
                           extendfrac=0.01,
-                          ticks=cbticks,
-                          format=ticker.LogFormatter(base=10.0, labelOnlyBase=False)
+                          **kw
         )
-    else:
-        CB = fig.colorbar(TT, ax=ax,
-                          pad=0.0,
-                          extendrect=True,
-                          extend='both',
-                          extendfrac=0.01
-        )
-    CB.ax.tick_params(which='major', length=0)
     CB.set_label(cblabel)
+    CB.ax.tick_params(which='major', length=0)
     CB.outline.set_linewidth(blw)
     CB.cmap.set_under(color='k')
     CB.cmap.set_over(color='w')
@@ -231,7 +263,7 @@ def setColorBar(TT,fig,ax,log=False,blw=1.0,cblabel=r"$z$",subs=[1.0]):
 #           EXTRACTING DATA
 ############################################################################
 
-def dataExtract3col(filename, N, cols=(0,1,2)):
+def dataExtract3col(filename, N, cols=(0,1,2),rescale=False):
     """contourExtract4(filename, N, cols=(0,1,2))
 
     Four columns extraction routine. Getting data ready for contour
@@ -246,14 +278,14 @@ def dataExtract3col(filename, N, cols=(0,1,2)):
     cols: tuple
     Tuple of three int referring to the columns to be read.
     """
-    #import numpy as np
+    import numpy as np
     import scipy.interpolate
     v1, v2, tt = np.loadtxt(filename, usecols=cols, unpack=True)
     v1i = np.linspace(v1.min(), v1.max(), N)
     v2i = np.linspace(v2.min(), v2.max(), N)
     ti = scipy.interpolate.griddata((v1, v2), tt,
                                     (v1i[None,:], v2i[:,None]),
-                                    method='cubic', rescale=True
+                                    method='cubic', rescale=rescale
     )
     return v1i, v2i, ti
 